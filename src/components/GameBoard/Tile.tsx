b11be28bfadb6c68c1b56cb4707a0e39fbab37e9
JoshM-Yoru <jmaciel335i@gmail.com>
2023-01-26 21:13:45 -0500

    placed key interactions inside a useEffect, for proper handling of keypresses

--- src/components/GameBoard/Tile.tsx
+++ src/components/GameBoard/Tile.tsx
@@ -16,35 +16,37 @@ const Tile: React.FC<TileProps> = ({ ground, description, damage, exhaustion, ti
 
     const playerRef = useRef<HTMLDivElement>(null);
 
-    if (tilePosition == playerPosition) {
+    useEffect(() => {
+        if (tilePosition == playerPosition) {
 
-        document.onkeydown = function(e) {
-            if (e.code === 'ArrowUp' || e.code === 'KeyK') {
-                playerPosition - 100 >= 0 && setPlayerPosition(playerPosition - 100);
-                playerPosition - 100 >= 0 && setHealth(health - gameBoard[playerPosition - 100].props.damage);
-                playerPosition - 100 >= 0 && setMovement(movement - gameBoard[playerPosition - 100].props.exhaustion);
-                playerRef.current?.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
-            }
-            if (e.code === 'ArrowDown' || e.code === 'KeyJ') {
-                playerPosition + 100 < 10000 && setPlayerPosition(playerPosition + 100);
-                playerPosition + 100 < 10000 && setHealth(health - gameBoard[playerPosition + 100].props.damage);
-                playerPosition + 100 < 10000 && setMovement(movement - gameBoard[playerPosition + 100].props.exhaustion);
-                playerRef.current?.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
-            }
-            if (e.code === 'ArrowLeft' || e.code === 'KeyH') {
-                playerPosition % 100 !== 0 && playerPosition - 1 >= 0 && setPlayerPosition(playerPosition - 1);
-                playerPosition % 100 !== 0 && setHealth(health - gameBoard[playerPosition - 1].props.damage);
-                playerPosition % 100 !== 0 && setMovement(movement - gameBoard[playerPosition - 1].props.exhaustion);
-                playerRef.current?.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
-            }
-            if (e.code === 'ArrowRight' || e.code === 'KeyL') {
-                (playerPosition + 1) % 100 !== 0 && playerPosition + 1 < 10000 && setPlayerPosition(playerPosition + 1);
-                (playerPosition + 1) % 100 !== 0 && playerPosition + 1 < 10000 && setHealth(health - gameBoard[playerPosition + 1].props.damage);
-                (playerPosition + 1) % 100 !== 0 && playerPosition + 1 < 10000 && setMovement(movement - gameBoard[playerPosition + 1].props.exhaustion);
-                playerRef.current?.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
+            document.onkeydown = function(e) {
+                if (e.code === 'ArrowUp' || e.code === 'KeyK') {
+                    playerPosition - 100 >= 0 && setPlayerPosition(playerPosition - 100);
+                    playerPosition - 100 >= 0 && setHealth(health - gameBoard[playerPosition - 100].props.damage);
+                    playerPosition - 100 >= 0 && setMovement(movement - gameBoard[playerPosition - 100].props.exhaustion);
+                    playerRef.current?.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
+                }
+                if (e.code === 'ArrowDown' || e.code === 'KeyJ') {
+                    playerPosition + 100 < 10000 && setPlayerPosition(playerPosition + 100);
+                    playerPosition + 100 < 10000 && setHealth(health - gameBoard[playerPosition + 100].props.damage);
+                    playerPosition + 100 < 10000 && setMovement(movement - gameBoard[playerPosition + 100].props.exhaustion);
+                    playerRef.current?.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
+                }
+                if (e.code === 'ArrowLeft' || e.code === 'KeyH') {
+                    playerPosition % 100 !== 0 && playerPosition - 1 >= 0 && setPlayerPosition(playerPosition - 1);
+                    playerPosition % 100 !== 0 && setHealth(health - gameBoard[playerPosition - 1].props.damage);
+                    playerPosition % 100 !== 0 && setMovement(movement - gameBoard[playerPosition - 1].props.exhaustion);
+                    playerRef.current?.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
+                }
+                if (e.code === 'ArrowRight' || e.code === 'KeyL') {
+                    (playerPosition + 1) % 100 !== 0 && playerPosition + 1 < 10000 && setPlayerPosition(playerPosition + 1);
+                    (playerPosition + 1) % 100 !== 0 && playerPosition + 1 < 10000 && setHealth(health - gameBoard[playerPosition + 1].props.damage);
+                    (playerPosition + 1) % 100 !== 0 && playerPosition + 1 < 10000 && setMovement(movement - gameBoard[playerPosition + 1].props.exhaustion);
+                    playerRef.current?.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
+                }
             }
         }
-    }
+    }, [playerPosition, movement, health])
 
     const centerPlayer = () => {
         if (playerRef.current) {
