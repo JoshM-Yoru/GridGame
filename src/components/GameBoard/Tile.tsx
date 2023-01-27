2cbcc25ae60f7a05dbd69792a1894cc6c6fd8a1a
JoshM-Yoru <jmaciel335i@gmail.com>
2023-01-27 10:39:51 -0500

    removed redundant code in Tile.tsx

--- src/components/GameBoard/Tile.tsx
+++ src/components/GameBoard/Tile.tsx
@@ -16,47 +16,41 @@ const Tile: React.FC<TileProps> = ({ ground, description, damage, exhaustion, ti
 
     const playerRef = useRef<HTMLDivElement>(null);
 
+
+    const centerPlayer = () => {
+        playerRef.current?.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
+    }
+
     useEffect(() => {
         if (tilePosition == playerPosition) {
 
             document.onkeydown = function(e) {
-                if (e.code === 'ArrowUp' || e.code === 'KeyK') {
-                    playerPosition - 100 >= 0 && setPlayerPosition(playerPosition - 100);
-                    playerPosition - 100 >= 0 && setHealth(health - gameBoard[playerPosition - 100].props.damage);
-                    playerPosition - 100 >= 0 && setMovement(movement - gameBoard[playerPosition - 100].props.exhaustion);
-                    playerRef.current?.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
+                if (playerPosition - 100 >= 0 && e.code === 'ArrowUp' || e.code === 'KeyK') {
+                    setPlayerPosition(playerPosition - 100);
+                    setHealth(health - gameBoard[playerPosition - 100].props.damage);
+                    setMovement(movement - gameBoard[playerPosition - 100].props.exhaustion);
                 }
-                if (e.code === 'ArrowDown' || e.code === 'KeyJ') {
-                    playerPosition + 100 < 10000 && setPlayerPosition(playerPosition + 100);
-                    playerPosition + 100 < 10000 && setHealth(health - gameBoard[playerPosition + 100].props.damage);
-                    playerPosition + 100 < 10000 && setMovement(movement - gameBoard[playerPosition + 100].props.exhaustion);
-                    playerRef.current?.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
+                if (playerPosition + 100 < 10000 && e.code === 'ArrowDown' || e.code === 'KeyJ') {
+                    setPlayerPosition(playerPosition + 100);
+                    setHealth(health - gameBoard[playerPosition + 100].props.damage);
+                    setMovement(movement - gameBoard[playerPosition + 100].props.exhaustion);
                 }
-                if (e.code === 'ArrowLeft' || e.code === 'KeyH') {
-                    playerPosition % 100 !== 0 && playerPosition - 1 >= 0 && setPlayerPosition(playerPosition - 1);
-                    playerPosition % 100 !== 0 && setHealth(health - gameBoard[playerPosition - 1].props.damage);
-                    playerPosition % 100 !== 0 && setMovement(movement - gameBoard[playerPosition - 1].props.exhaustion);
-                    playerRef.current?.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
+                if (playerPosition % 100 !== 0 && playerPosition - 1 >= 0 && e.code === 'ArrowLeft' || e.code === 'KeyH') {
+                    setPlayerPosition(playerPosition - 1);
+                    setHealth(health - gameBoard[playerPosition - 1].props.damage);
+                    setMovement(movement - gameBoard[playerPosition - 1].props.exhaustion);
                 }
-                if (e.code === 'ArrowRight' || e.code === 'KeyL') {
-                    (playerPosition + 1) % 100 !== 0 && playerPosition + 1 < 10000 && setPlayerPosition(playerPosition + 1);
-                    (playerPosition + 1) % 100 !== 0 && playerPosition + 1 < 10000 && setHealth(health - gameBoard[playerPosition + 1].props.damage);
-                    (playerPosition + 1) % 100 !== 0 && playerPosition + 1 < 10000 && setMovement(movement - gameBoard[playerPosition + 1].props.exhaustion);
-                    playerRef.current?.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
+                if ((playerPosition + 1) % 100 !== 0 && playerPosition + 1 < 10000 && e.code === 'ArrowRight' || e.code === 'KeyL') {
+                    setPlayerPosition(playerPosition + 1);
+                    setHealth(health - gameBoard[playerPosition + 1].props.damage);
+                    setMovement(movement - gameBoard[playerPosition + 1].props.exhaustion);
                 }
             }
         }
-    }, [playerPosition, movement, health])
 
-    const centerPlayer = () => {
-        if (playerRef.current) {
-            playerRef.current.scrollIntoView({ behavior: "smooth", block: "center", inline: "center" });
-        }
-    }
-
-    useEffect(() => {
         centerPlayer();
-    }, [])
+
+    }, [playerPosition, movement, health])
 
     return (
         <div onClick={centerPlayer} className='tile-container' style={{ backgroundImage: `url(${ground})` }}>
